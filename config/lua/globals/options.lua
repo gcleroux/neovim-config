53d0f077a77d2be6ffb85e91c0b66cf3cf3c754c
Guillaume Cleroux <guicleroux@icloud.com>
2023-05-29 15:23:14 -0400

    Comment formatting

--- config/lua/globals/options.lua
+++ config/lua/globals/options.lua
@@ -29,7 +29,7 @@ o.cursorline = true
 o.termguicolors = true
 o.showmode = false
 o.foldcolumn = "0"
-o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+o.foldlevel = 99      -- Using ufo provider need a large value, feel free to decrease the value
 o.foldlevelstart = 99 -- Starting at foldlevel 99 makes all code visible (unfolded)
 o.foldenable = true
 
