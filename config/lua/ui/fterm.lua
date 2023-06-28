721992ef086b240ca51dcc7ab0d32a5baad3ecf6
Guillaume Cleroux <guicleroux@icloud.com>
2023-06-28 09:20:25 -0400

    Replace toggleterm with fterm

--- config/lua/ui/fterm.lua
+++ config/lua/ui/fterm.lua
@@ -11,3 +11,30 @@ fterm.setup({
         width = 0.9,
     },
 })
+
+local btop = fterm:new({
+    ft = "btop", -- You can also override the default filetype, if you want
+    cmd = "btop",
+    dimensions = {
+        height = 0.9,
+        width = 0.9,
+    },
+})
+
+local zathura = fterm:new({
+    ft = "zathura", -- You can also override the default filetype, if you want
+    cmd = "zathura " .. vim.fn.expand("%:p"),
+    dimensions = {
+        height = 0.9,
+        width = 0.9,
+    },
+})
+
+-- Use this to toggle btop in a floating terminal
+vim.keymap.set("n", "<A-b>", function()
+    btop:toggle()
+end)
+
+vim.keymap.set("n", "<A-z>", function()
+    zathura:toggle()
+end)
